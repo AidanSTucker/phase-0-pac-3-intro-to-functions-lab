14837bfa7dc667095c21f9de3740631dfa661e6d
elpatron <aidanseantucker@gmail.com>
2023-04-04 16:55:58 -0400

    completed 5/6 challenges

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  
+  function logShout(string) {
+
+  console.log(string);
+    console.log(string.toUpperCase());
+    
+    console.log(string === string.toUpperCase());
+}
+  
+
+
+function logWhisper(string) {
+
+    console.log(string);
+      console.log(string.toLowerCase());
+      
+      console.log(string === string.toLowerCase());
+  }
+
+function sayHiToHeadphonedRoommate(string) { 
+if(string === string.toLowerCase);
+return("I can't hear you!") 
+}
+
+
+
+
+
+
+
+
+
