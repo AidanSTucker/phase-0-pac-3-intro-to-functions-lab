fbb7b887c4970973518646d21a8a2ddf08f9ca82
elpatron <aidanseantucker@gmail.com>
2023-05-05 11:59:16 -0400

    fixed the silly typo

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 function shout(string) {
-    return string.toUpperCase();
-  }
-
-  function whisper(string) {
-    return string.toLowerCase();
-  }
-  
-  function logShout(string) {
-
-  console.log(string);
-    console.log(string.toUpperCase());
-    
-    console.log(string === string.toUpperCase());
+  return string.toUpperCase();
 }
-  
 
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
 
 function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
 
-    console.log(string);
-      console.log(string.toLowerCase());
-      
-      console.log(string === string.toLowerCase());
+function sayHiToHeadphonedRoommate(string) {
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  } else if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  } else {
+    return "I would love to!";
   }
-
-function sayHiToHeadphonedRoommate(string) { 
-if(string === string.toLowerCase);
-return("I can't hear you!") 
 }
 
 
